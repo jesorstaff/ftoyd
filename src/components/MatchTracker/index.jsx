7ce904e3263e1102030dc8acfd753156b2dffe50
Timur Bairamgulov <jesorstaff@gmail.com>
2025-03-11 17:38:24 +0500

    add logic for update button

--- src/components/MatchTracker/index.jsx
+++ src/components/MatchTracker/index.jsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
+import useSWR, { mutate } from "swr";
 import MatchTrackerHeader from "./MatchTrackerHeader";
 import MatchTrackerList from "./MatchTrackerList";
-import useSWR, { mutate } from "swr";
 
 const fetcher = (...args) =>
   fetch(...args).then((res) => res.json().then((data) => data.data));
 
 const MatchTracker = () => {
-  const URL = "https://app.ftoyd.com/fronttemp-service/fronttemp/";
+  const URL = "https://app.ftoyd.com/fronttemp-service/fronttemp";
+  const { data: swrData, isLoading, error } = useSWR(URL, fetcher);
+  const [data, setData] = useState(swrData);
 
-  const { data, isLoading, error } = useSWR(URL, fetcher);
-
-  const refreshData = () => {
-    console.log("refreshData");
-    mutate(URL);
+  const refreshData = async () => {
+    try {
+      const newData = await fetcher(URL);
+      setData(newData);
+      mutate(URL, newData);
+    } catch (err) {
+      console.log("Ошибка при обновлении данных:", err);
+    }
   };
+
+  if (swrData && !data) {
+    setData(swrData);
+  }
 
   return (
     <div className="w-full mx-auto py-13 px-[42px]">
-      <MatchTrackerHeader
-        error={error}
-        isLoading={isLoading}
-        refreshData={refreshData}
-      />
-      <MatchTrackerList data={data} isLoading={isLoading} />
+      <MatchTrackerHeader error={error} refreshData={refreshData} />
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        <MatchTrackerList data={data} isLoading={isLoading} />
+      )}
     </div>
   );
 };
--- src/components/ui/ButtonUI.jsx
+++ src/components/ui/ButtonUI.jsx
@@ -1,10 +1,11 @@
 import update from "../../assets/update.svg";
 
-const ButtonUI = ({ label, onClick }) => {
+const ButtonUI = ({ label, onClick, disabled = false }) => {
   return (
     <button
       className="flex items-center gap-2 bg-default-red active:bg-pressed-red disabled:bg-disabled-red rounded-sm w-fit px-10 py-4 cursor-pointer"
       onClick={onClick}
+      disabled={disabled}
     >
       {label}
       <img src={update} alt={label} />
